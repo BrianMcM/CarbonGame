25cc917458a4e58b1f318ddb4f8a1cb6b36670f8
thomaspollock123 <thomas.pollock@ucdconnect.ie>
2024-04-15 13:48:22 +0100

    map files (map_final and metro_final = newest)

---
+++ assets/testMap/roguelikeCity_magenta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="roguelikeCity_magenta" tilewidth="16" tileheight="16" margin="5" tilecount="1102" columns="38">
+ <image source="../../../../OneDrive/Desktop/Programming/Semester 2/Java Programming/Visual Assets/Roguelike Modern City pack/Spritesheet/roguelikeCity_magenta.png" width="628" height="475"/>
+</tileset>
