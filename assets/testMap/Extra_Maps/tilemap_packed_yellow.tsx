712b6a0c993028a60d401fccf0136cf2a48b67ef
thomaspollock123 <thomas.pollock@ucdconnect.ie>
2024-04-23 12:58:13 +0100

    Map update, bus/train highlights tmx files added to "testMap/Hover_routes"

--- assets/testMap/Extra_Maps/tilemap_packed_yellow.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilemap_packed_yellow" tilewidth="16" tileheight="16" tilecount="486" columns="27">
- <image source="tilemap_packed_yellow.png" width="432" height="288"/>
-</tileset>
