304585a669f9e628edf5ccb3298f39c72ae270ac
thomaspollock123 <thomas.pollock@ucdconnect.ie>
2024-04-26 12:23:31 +0100

    200 second level time

--- core/src/com/carbon/game/CarbonGame.java
+++ core/src/com/carbon/game/CarbonGame.java
@@ -33,11 +33,11 @@ public class CarbonGame extends Game {
 				newScreen = new ScoreScreen(this);
 				break;
 			case 4:
-				newScreen = new GameScreen(this, "Maps/map_tutorial.tmx", "Maps/metro_tutorial.tmx", 20,"      Tutorial");
+				newScreen = new GameScreen(this, "Maps/map_tutorial.tmx", "Maps/metro_tutorial.tmx", 200,"      Tutorial");
 				screen_replay = screenNumber;
 				break;
 			default:
-				newScreen = new GameScreen(this, "Maps/map.tmx", "Maps/metro.tmx", 20, "      Level ONE");
+				newScreen = new GameScreen(this, "Maps/map.tmx", "Maps/metro.tmx", 200, "      Level ONE");
 				screen_replay = screenNumber;
 		}
 		screenChange(newScreen);
