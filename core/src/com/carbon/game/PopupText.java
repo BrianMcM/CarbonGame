b4b1f22af389696905cd7596e88a6cd04f7715da
Adam Southwick <adamsouthwick100@gmail.com>
2024-04-26 08:43:36 +0100

    tiny fixes

--- core/src/com/carbon/game/PopupText.java
+++ core/src/com/carbon/game/PopupText.java
@@ -3,7 +3,7 @@ package com.carbon.game;
 public class PopupText {
     String gemPopup = "Collecting Gems gives you score. \nGain the most score in the allotted \ntime to gain places on the leaderboard";
     String carbonPopup = "Using transport such as bus, \ntrain or taxis creates carbon.  \nCarbon is bad for the environment \nand increases your score\nor even kill your player and end the game";
-    String carbonPopup_2 = "Your Carbon Score has reached 500\nYou are close to the carbon threshold that will end the game\nBe careful what transport methods you use\n If you reach 1000 carbon you will DIE!";
+    String carbonPopup_2 = "You are close to the carbon threshold that will end the game\nBe careful what transport methods you use\n If you reach 3000 carbon it's GAME OVER!";
     String energyPopup = "You're low on energy. \nWalking and Biking is tiring \nand causes your energy to drop.  Wait to \nrecover your energy or use \nanother transport option";
     String timePopup = "You're halfway through your time.\n" +
             "Keep collecting gems and balance your carbon use to score max points\n";
